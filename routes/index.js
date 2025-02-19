453df3d07db7c3b3371475195df7f8965439ad0b
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2025-02-19 12:34:56 +0300

    Add filter for stations

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,27 @@ const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
+//Filter for "stations" route
+const filter = (data) => {
+  const dataObj = JSON.parse(data);
+
+  const rus = dataObj["countries"].find(
+    (country) => country.title === "Россия"
+  );
+
+  const stations = [];
+
+  for (let region of rus.regions) {
+    for (let settlement of region.settlements) {
+      for (let station of settlement.stations) {
+        if (station.station_type === "train_station") stations.push(station);
+      }
+    }
+  }
+
+  return stations;
+};
+
 router.use(
   cors({
     origin: "*",
@@ -25,17 +46,19 @@ router.get("/:route", async (req, res) => {
 
     const apiRes = await needle(
       "GET",
-      `${API_BASE_URL}${reqRoute}/?${params}&lang=ru_RU&format=json`
+      `${API_BASE_URL}${reqRoute}/?lang=ru_RU&format=json&${params}`
     );
     const data = apiRes.body;
 
-    // if (process.env.NODE_ENV !== "production") {
-    //   console.log(`REQUEST: ${API_BASE_URL}${reqRoute}/?${params}`);
-    // }
+    if (reqRoute === "stations_list") {
+      const stations = filter(data);
+      res.status(200).json(stations);
+      return;
+    }
 
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error });
+    res.status(500).json({ error: error.message });
   }
 });
 
